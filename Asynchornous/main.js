f616a1788674eebc6aa1351ec257acb1fc16b55b
RidhoFahrizal <farizalridho@gmail.com>
2025-01-07 23:48:15 +0700

    update materi async

---
+++ Asynchornous/main.js
@@ -0,0 +1,14 @@
+import { makeCoffee, membayar } from './coffe.js';
+
+console.log('Saya memesan kopi di kafe.');
+
+makeCoffee();
+
+console.log('Pramusaji memberikan kopi pesanan.');
+console.log('Saya mendapatkan kopi dan menghabiskannya.');
+
+
+
+membayar();
+console.log('totalnya 7000 rupiah kak :');
+console.log('baik sebentar ya (membuka dompet)');
